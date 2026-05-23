72047a15e25dedae9619c8c0b89c3a7d39c5ed9b
Gigabite1277 <neiltraining2024@gmail.com>
2026-05-23 12:24:58 +0000

    DEBUG set to False

--- wots/settings.py
+++ wots/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vkq-qek_=&$*e0fr6v=p(@dy#%7aergjed5k6st$9#d%enmxe("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['.herokuapp.com',  ]
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', ]
 
 
 # Application definition
