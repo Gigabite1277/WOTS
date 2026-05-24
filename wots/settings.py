18811703dea12293e04f4dcd6168cfefcafbf55f
Gigabite1277 <neiltraining2024@gmail.com>
2026-05-24 15:18:10 +0000

    Setting up databases yet unable to migrate tables

--- wots/settings.py
+++ wots/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,31 +79,36 @@ WSGI_APPLICATION = "wots.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get("postgresql://neondb_owner:npg_kPoyKBpV8Hi1@ep-royal-dew-agmrmk27.c-2.eu-central-1.aws.neon.tech/table_frail_mace_681248"))
 }
+
 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
+#AUTH_PASSWORD_VALIDATORS = [
+#   {
+#        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+#   },
+#    {
+#        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+#    },
+#    {
+#        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+#    },
+#    {
+#        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+#    },
+#]
 
 
 # Internationalization
