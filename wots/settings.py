5061d61ee5a880cf1788ca4f1dea5f1615c54555
Gigabite1277 <neiltraining2024@gmail.com>
2026-09-13 16:51:57 +0000

    Managed to recover fixtures from Heroku salvaged DB is malformed, so no users

--- wots/settings.py
+++ wots/settings.py
@@ -25,10 +25,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY") or "django-insecure-local-development-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
@@ -111,8 +111,11 @@ WSGI_APPLICATION = "wots.wsgi.application"
 #    }
 #}
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(
+        DATABASE_URL or f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 CSRF_TRUSTED_ORIGINS = [
