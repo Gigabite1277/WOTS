3a28901bcfd676380030ea4a60232dcde6407709
Gigabite1277 <neiltraining2024@gmail.com>
2026-09-20 23:50:01 +0000

    Downloaed user database from Heroku

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User # Import models to connect
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
