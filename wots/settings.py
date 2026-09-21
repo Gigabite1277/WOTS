4a591f81a4b942dacebe468da02f8d7da069abb7
Gigabite1277 <neiltraining2024@gmail.com>
2026-09-21 15:11:12 +0000

    enable serving of image files

--- wots/settings.py
+++ wots/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY") or "django-insecure-local-development-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
